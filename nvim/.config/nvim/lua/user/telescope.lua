dabc13ae93537d6f98114fdea644eb0ad8c635fd
AshishKatale <kataleashish999@gmail.com>
2023-03-26 11:55:10 +0530

    Refactored telescope keymaps

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -12,11 +12,86 @@ telescope.load_extension('undo');
 
 local actions = require "telescope.actions"
 
+local mappings = {
+	i = {
+		["<C-n>"] = actions.cycle_history_next,
+		["<C-p>"] = actions.cycle_history_prev,
+
+		["<Tab>"] = actions.move_selection_next,
+		["<S-Tab>"] = actions.move_selection_previous,
+
+		["<C-c>"] = actions.close,
+
+		["<Down>"] = actions.move_selection_next,
+		["<Up>"] = actions.move_selection_previous,
+
+		["<CR>"] = actions.select_default,
+		["<C-x>"] = actions.select_horizontal,
+		["<C-v>"] = actions.select_vertical,
+		["<C-t>"] = actions.select_tab,
+
+		["<C-u>"] = actions.preview_scrolling_up,
+		["<C-d>"] = actions.preview_scrolling_down,
+
+		["<PageUp>"] = actions.results_scrolling_up,
+		["<PageDown>"] = actions.results_scrolling_down,
+
+		["<C-j>"] = actions.toggle_selection + actions.move_selection_worse,
+		["<C-k>"] = actions.toggle_selection + actions.move_selection_better,
+		-- ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+		-- ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+		["<C-q>"] = trouble.open_with_trouble,
+		["<M-q>"] = trouble.open_selected_with_trouble,
+		["<C-l>"] = actions.complete_tag,
+		["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+	},
+
+	n = {
+		["<C-c>"] = actions.close,
+		["<CR>"] = actions.select_default,
+		["l"] = actions.select_default,
+		["<C-x>"] = actions.select_horizontal,
+		["<C-v>"] = actions.select_vertical,
+		["<C-t>"] = actions.select_tab,
+
+		["<C-j>"] = actions.toggle_selection + actions.move_selection_worse,
+		["<C-k>"] = actions.toggle_selection + actions.move_selection_better,
+		-- ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+		-- ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+		["<C-q>"] = trouble.open_with_trouble,
+		["<M-q>"] = trouble.open_selected_with_trouble,
+
+		["Tab"] = actions.move_selection_next,
+		["S-Tab"] = actions.move_selection_previous,
+		["j"] = actions.move_selection_next,
+		["k"] = actions.move_selection_previous,
+		["H"] = actions.move_to_top,
+		["M"] = actions.move_to_middle,
+		["L"] = actions.move_to_bottom,
+
+		["<Down>"] = actions.move_selection_next,
+		["<Up>"] = actions.move_selection_previous,
+		["gg"] = actions.move_to_top,
+		["G"] = actions.move_to_bottom,
+
+		["<C-u>"] = actions.preview_scrolling_up,
+		["<C-d>"] = actions.preview_scrolling_down,
+
+		["<PageUp>"] = actions.results_scrolling_up,
+		["<PageDown>"] = actions.results_scrolling_down,
+
+		["?"] = actions.which_key,
+	},
+}
 telescope.setup {
   defaults = {
 		layout_config = {
-      vertical = { width = 0.5 },
 			prompt_position = "top",     -- other layout configuration here
+			horizontal = {
+				height = 0.9,
+				preview_cutoff = 120,
+				width = 0.8
+			},
     },
 		sorting_strategy="ascending",
     prompt_prefix = "   ",
@@ -29,86 +104,9 @@ telescope.setup {
 			"%.mkv", "%.mp4", "%.zip",
 			"node_modules/",
 		},
-    mappings = {
-      i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-
-        ["<Tab>"] = actions.move_selection_next,
-        ["<S-Tab>"] = actions.move_selection_previous,
-
-        ["<C-c>"] = actions.close,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["<C-j>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<C-k>"] = actions.toggle_selection + actions.move_selection_better,
-        -- ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-				-- ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-q>"] = trouble.open_with_trouble,
-        ["<M-q>"] = trouble.open_selected_with_trouble,
-        ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-      },
-
-      n = {
-        ["<C-c>"] = actions.close,
-        ["<CR>"] = actions.select_default,
-        ["l"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-j>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<C-k>"] = actions.toggle_selection + actions.move_selection_better,
-        -- ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        -- ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-q>"] = trouble.open_with_trouble,
-        ["<M-q>"] = trouble.open_selected_with_trouble,
-
-        ["Tab"] = actions.move_selection_next,
-        ["S-Tab"] = actions.move_selection_previous,
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["?"] = actions.which_key,
-      },
-    },
+    mappings = mappings,
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
 		buffers = {
 			initial_mode = "normal",
 		},
@@ -121,10 +119,6 @@ telescope.setup {
 		}
   },
   extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
 		fzf = {
       fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = true,  -- override the generic sorter
