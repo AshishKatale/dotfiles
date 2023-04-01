403aee218c5bd0fc8692a12357aafcd4c29bc2e1
AshishKatale <kataleashish999@gmail.com>
2023-04-01 16:24:25 +0530

    added treesitter context

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -90,6 +90,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+	use 'nvim-treesitter/nvim-treesitter-context'
 
 	use 'nvim-tree/nvim-tree.lua'
 
