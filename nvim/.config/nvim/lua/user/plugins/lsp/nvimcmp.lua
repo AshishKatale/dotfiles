ba95901e27f4033d0772462cb888db78df4e0da4
AshishKatale <kataleashish999@gmail.com>
2023-04-09 10:56:04 +0530

    changed autocompletion keybind

--- nvim/.config/nvim/lua/user/plugins/lsp/nvimcmp.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/nvimcmp.lua
@@ -58,7 +58,7 @@ cmp.setup {
 		["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<F16>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ["<C-c>"] = cmp.mapping {
       i = cmp.mapping.abort(),
