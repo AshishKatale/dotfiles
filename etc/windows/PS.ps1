8365e1b3109101b6d3ee47120d077b15b57fb250
Ashish Katale <61214878+AshishKatale@users.noreply.github.com>
2026-06-23 20:59:11 +0530

    update powershell config

--- etc/windows/PS.ps1
+++ etc/windows/PS.ps1
@@ -2,7 +2,6 @@ Set-Alias -Name c -Value clear
 Set-Alias -Name ll -Value ls
 Set-Alias -Name opn -Value explorer
 
-function xxx { exit }
 function D { cd D:\ }
 function C { cd C:\ }
 function psrc { code $PROFILE }
@@ -20,37 +19,46 @@ function ndv { npm run dev }
 function nbld { npm run build }
 
 function GIT_INFO {
-	($REPO_EXISTS = git rev-parse --is-inside-work-tree) > $null
-	if ($REPO_EXISTS) {
-		($BRANCH = git branch --show-current) > $null
-		if($BRANCH){
-			$ICON = "$([char]0xdb80)$([char]0xdea2)" # 󰊢
-			Write-Host " $ICON $BRANCH" -ForegroundColor "Green" -NoNewline
-			return
-		}
+  try {
+    git --version | Out-Null
+  }
+  catch [System.Management.Automation.CommandNotFoundException] {
+    return
+  }
 
-		($TAG = git tag --points-at=HEAD) > $null
-		if($TAG){
-			$ICON = "$([char]0xdb81)$([char]0xdcfb)" # 󰓻
-			Write-Host " $ICON $TAG" -ForegroundColor "Green" -NoNewline
-			return
-		}
+  ($REPO_EXISTS = git rev-parse --is-inside-work-tree) > $null
+  if ($REPO_EXISTS) {
+    ($BRANCH = git branch --show-current) > $null
+    if($BRANCH){
+      $ICON = "$([char]0xdb80)$([char]0xdea2)" # 󰊢
+      Write-Host " $ICON $BRANCH" -ForegroundColor "Green" -NoNewline
+      return
+    }
 
-		($COMMIT = git rev-parse --short HEAD) > $null
-		if($COMMIT) {
-			$ICON = "$([char]0xf417)" # 
-			Write-Host " $ICON $COMMIT " -ForegroundColor "Green" -NoNewline
-		}
-	}
+    ($TAG = git tag --points-at=HEAD) > $null
+    if($TAG){
+      $ICON = "$([char]0xdb81)$([char]0xdcfb)" # 󰓻
+      Write-Host " $ICON $TAG" -ForegroundColor "Green" -NoNewline
+      return
+    }
+
+    ($COMMIT = git rev-parse --short HEAD) > $null
+    if($COMMIT) {
+      $ICON = "$([char]0xf417)" # 
+      Write-Host " $ICON $COMMIT " -ForegroundColor "Green" -NoNewline
+    }
+  }
 }
 
 $PROMPT_STYLE="fancy"
 function prompt {
-	Write-Host "$PWD" -ForegroundColor "Cyan" -NoNewline
+  Write-Host "$PWD" -ForegroundColor "Cyan" -NoNewline
   if($PROMPT_STYLE.Equals("fancy")) {
     GIT_INFO
+    Write-Host " $" -ForegroundColor "Yellow" -NoNewline
+    return " "
   }
-	Write-Host " $" -ForegroundColor "Yellow" -NoNewline
-	return " "
+  Write-Host " $" -ForegroundColor "Yellow" -NoNewline
+  return " "
 }
 
